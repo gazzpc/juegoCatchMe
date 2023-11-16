ebb3678bc605021f25a31accb7a297265557b9ed
Gaston Paz Cinello <gastonpazcinello@gmail.com>
2023-11-16 01:43:31 -0300

    juego catchMe funcionando para pc

---
+++ js/app.js
@@ -0,0 +1,39 @@
+let target = document.getElementById("target");
+let scoreDisplay = document.getElementById("score");
+let speedDisplay = document.getElementById("speed");
+let score = 0;
+let speed = 0;
+
+target.addEventListener("click", function() {
+  // touchstart: dispositivos moviles y click: computadora
+  score++;
+  speed++;
+  scoreDisplay.innerHTML = score;
+  speedDisplay.innerHTML = speed
+  moveTarget();
+});
+
+const containerWidth = document.getElementById("game-container").offsetWidth;
+const containerHeight = document.getElementById("game-container").offsetHeight;
+const targetSize = target.offsetWidth;
+const maxX = containerWidth - targetSize;
+const maxY = containerHeight - targetSize; 
+
+function moveTarget() {
+  
+  let randomX = Math.floor(Math.random() * maxX);
+  let randomY = Math.floor(Math.random() * maxY);
+  target.style.left = randomX + "px";
+  target.style.top = randomY + "px";
+
+  setTimeout(moveTarget, 2000 / speed);
+  console.log("random x: ", randomX);
+  console.log("random y: ", randomy);
+}
+
+// function increaseSpeed() {
+//   if (score % 5 === 0) {
+//     speed++;
+//   }
+// }
+  
